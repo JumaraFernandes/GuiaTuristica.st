3d17b63be9820380be82f6d04004ded72ff29b31
Jumara Fernandes <80683054+JumaraFernandes@users.noreply.github.com>
2023-05-23 23:31:33 +0100

    COMIT

--- hoteis.php
+++ hoteis.php
@@ -16,19 +16,113 @@
   <!--cabeçalho-->
   <?php include 'includes/header.php' ?>
     <div class="posicao">
-    <h2>Hotéis e Resorts na ilha de São Tomé e Principe</h2>
+    <h4>Hotéis e Resorts na ilha de São Tomé e Principe</h4>
     </div>
   <main> 
     <div class="corpo">
       <h1>Hoteis</h1>
     </div>
 
-      <section>
-         
+      <section class="saotome">
+         <div>
+             <h1>Hotéis e Resorts na ilha de São Tomé</h1>
+           <p>
+             Em termos de estruturas hoteleiras, a cidade de São Tomé tem alguns hotéis e resorts de qualidade a bom nível. O hotel Miramar, na marginal 12 Julho é um clássico da cidade. Inaugurado nos anos oitenta, foi durante muito tempo o único com condições para receber homens de negócios e visitantes importantes, por isso construiu uma sólida reputação.
+           </p>
+             <p>
+             O Omali Lodge e o Hotel praia são outros refúgios de qualidade inquestionável: localizados na praia Lagarto. Ao extremo sul  encontra-se o Resort Inhame Ecolodge situado a 7 minutos de barco do marco do equador.
+             </p>
+             <div class="row">
+  <div class="col-md-4">
+    <div class="card">
+      <img src="imagens/hotelpestana.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5>PESTANA SÃO TOMÉ</h5>
+        <p class="card-text">Av. Marginal 12 de Julho</p>
+        <p class="card-text">Tel: (+239) 2244500/2244539</p>
+        <a class="card-text" href="mailto:pestana.stome@pestana.com">E-mail: pestana.stome@pestana.com</a>
+        <a class="card-text" href="https://www.pestana.com/pt" target="_blank">www.pestana.com</a>
+      </div>
+    </div>
+  </div>
+  
+  <div class="col-md-4">
+    <div class="card">
+      <img src="imagens/hotelpraia.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5>HOTEL PRAIA</h5>
+        <p class="card-text">Praia Lagarto</p>
+        <p class="card-text">Tel: (+239) 2226235/9986250</p>
+        <a class="card-text" href="reservas@hotelpraia.st">E-mail: reservas@hotelpraia.st</a>
+        <a class="card-text" href="https://www.hotel-praia.com" target="_blank">www.hotel-praia.com</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-4">
+    <div class="card">
+      <img src="imagens/hotelrocasanto.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5>HOTEL ROÇA SANTO ANTONIO</h5>
+        <p class="card-text">Roça de Santo António</p>
+        <p class="card-text">Tel: (+239)9986652</p>
+        <a class="card-text" href="serviruma.rocasantoantonio@gmail.com">E-mail: E-mail: serviruma.rocasantoantonio@gmail.com</a>
+        <a class="card-text" href="https://www.rsaecolodge.pt" target="_blank">www.rsaecolodge.pt</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-4">
+    <div class="card">
+      <img src="imagens/hotelcentral.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5>HOTEL CENTRAL</h5>
+        <p class="card-text">Rua de Angola</p>
+        <p class="card-text">Tel: (+239) 2227551</p>
+        <a class="card-text" href="hotelcentral.saotome@gmail.com">E-mail: hotelcentral.saotome@gmail.com</a>
+        <a class="card-text" href="https://www.hotelcentralsaotome.com" target="_blank">www.hotelcentralsaotome.com</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-4">
+    <div class="card">
+      <img src="imagens/compataag.jpeg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5>TAAG</h5>
+        <p class="card-text">Av. Marginal 12 de Julho</p>
+        <p class="card-text">Tel: (+239) 2241150</p>
+        <a class="card-text" href="mailto:taagstp@cstome.net">E-mail: taagstp@cstome.ne</a>
+        <a class="card-text" href="https://www.pestana.com/pt" target="_blank">www.pestana.com</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-4">
+    <div class="card">
+      <img src="imagens/compaTap.jpeg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5>TAP</h5>
+        <p class="card-text">Av. Marginal 12 de Julho</p>
+        <p class="card-text">Tel: (+239) 2222307/2223839</p>
+        <a class="card-text" href="mailto:tap@cstome.net">E-mail: tap@cstome.net</a>
+        <a class="card-text" href="https://www.pestana.com/pt" target="_blank">www.pestana.com</a>
+      </div>
+    </div>
+  </div>
+</div>   
+
+         </div>
 
       </section>
      
-       
+      <section class="pricinpe">
+         
+           <div>
+           <h1>Hoteis em Principe</h1>
+           </div>
+
+         </section>
      </div>
     </main>
 
